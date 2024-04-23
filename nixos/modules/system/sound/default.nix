0ff829916e3d04e6cd5a08871bb06ad70488f52f
Skyler Mäntysaari <samip5@users.noreply.github.com>
2024-04-23 07:13:29 +0300

    nixos: Sounds via bluetooth needs fixing..

--- nixos/modules/system/sound/default.nix
+++ nixos/modules/system/sound/default.nix
@@ -14,16 +14,16 @@ in {
       alsa.support32Bit = true;
       pulse.enable = true;
     };
-    environment.etc = mkIf device.hasBluetooth {
-      "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-        bluez_monitor.properties = {
-          ["bluez5.enable-sbc-xq"] = true,
-          ["bluez5.enable-msbc"] = true,
-          ["bluez5.enable-hw-volume"] = true,
-          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        }
-      '';
-    };
+    #environment.etc = mkIf device.hasBluetooth {
+    #  "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+    #    bluez_monitor.properties = {
+    #      ["bluez5.enable-sbc-xq"] = true,
+    #      ["bluez5.enable-msbc"] = true,
+    #      ["bluez5.enable-hw-volume"] = true,
+    #      ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+    #    }
+    #  '';
+    #};
   };
 }
 
